a6ec0b28b00e8eb03fceb2e51415ccf583d5e479
Jan Schöpke <janschoepke@ymail.com>
2017-08-03 09:48:43 +0200

    Added possibility to add SMTP Server to mailing.

--- src/views/EditMailing.vue
+++ src/views/EditMailing.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="animated fadeIn">
 
+    <transition name="fade">
+      <div class="card card-inverse card-danger text-xs-center" v-if="errorText">
+        <div class="card-block">
+          <blockquote class="card-blockquote">
+            <i class="fa fa-warning"></i> {{errorText}}
+          </blockquote>
+        </div>
+      </div>
+    </transition>
+
     <div class="card">
       <div class="card-header">
         <strong>{{ mode }} Mailing</strong>
@@ -30,16 +40,56 @@
           <label for="from-name">From Name</label>
           <input type="text" class="form-control" id="from-name" placeholder="From Name" v-model="from_name">
         </div>
-
-        <div class="checkbox">
-          <label for="tracking">
-            <input type="checkbox" id="tracking" name="tracking" value="true" v-model="tracking"> Website-Tracking?
-          </label>
+        <div class="row">
+          <div class="col-md-6">
+            <label for="smtpSwitch">SMTP Server Connection?</label><br>
+            <label class="switch switch-text switch-primary">
+              <input type="checkbox" checked="checked" class="switch-input" v-model="isSmtp">
+              <span data-on="On" data-off="Off" class="switch-label"></span>
+              <span class="switch-handle"></span>
+            </label>
+          </div>
+          <div class="col-md-6">
+            <label for="trackingSwitch">Enable Onpage Tracking?</label><br>
+            <label class="switch switch-text switch-primary">
+              <input type="checkbox" checked="checked" class="switch-input" v-model="tracking">
+              <span data-on="On" data-off="Off" class="switch-label"></span>
+              <span class="switch-handle"></span>
+            </label>
+          </div>
         </div>
+
+        <transition name="fade">
+          <div v-if="isSmtp">
+            <div class="form-group">
+              <label for="smtpHost">SMTP Host</label>
+              <input type="text" class="form-control" id="smtpHost" placeholder="SMTP Host" v-model="smtpHost">
+            </div>
+            <div class="form-group">
+              <label for="smtpUsername">SMTP Username</label>
+              <input type="text" class="form-control" id="smtpUsername" placeholder="SMTP Username" v-model="smtpUser">
+            </div>
+            <div class="form-group">
+              <label for="smtpPassword">SMTP Password</label>
+              <input type="password" class="form-control" id="smtpPassword" placeholder="SMTP Password" v-model="smtpPassword">
+            </div>
+            <div class="form-group">
+              <label for="smtpSecure">SMTP Secure</label>
+              <input type="text" class="form-control" id="smtpSecure" placeholder="SMTP Secure" v-model="smtpSecure">
+            </div>
+            <div class="form-group">
+              <label for="smtpPort">SMTP Port</label>
+              <input type="number" class="form-control" id="smtpPort" placeholder="Port" v-model="smtpPort">
+            </div>
+          </div>
+        </transition>
 
       </div>
       <div class="card-footer">
-        <button type="submit" class="btn btn-sm btn-primary" v-on:click="handleFormSubmit()"><i class="fa fa-dot-circle-o"></i> Submit</button>
+        <button type="button" class="btn btn-sm btn-primary" v-on:click="handleFormSubmit()" :disabled="submitted"  :class="{'submitted': submitted}">
+          <span class="button-text">Submit</span>
+          <i class="fa fa-gear fa-spin"></i>
+        </button>
         <router-link to="/mailings" class="btn btn-sm btn-danger"><i class="fa fa-ban"></i> Back</router-link>
       </div>
     </div>
@@ -61,7 +111,13 @@
         submitted: false,
         errorText: '',
         currentRoute: '',
-        mode: 'Add'
+        mode: 'Add',
+        isSmtp: false,
+        smtpHost: '',
+        smtpUser: '',
+        smtpPassword: '',
+        smtpSecure: '',
+        smtpPort: ''
       }
     },
     created: function () {
@@ -98,6 +154,7 @@
       handleFormSubmit () {
         this.submitted = true
         if (!this.name || !this.headline || !this.content || !this.from_email || !this.from_name) {
+          window.scrollTo(0, 0)
           this.errorText = 'Please fill out all the required fields'
           this.submitted = false
           return
@@ -114,7 +171,13 @@
               fromName: this.from_name,
               subject: this.headline,
               body: this.content,
-              addTracking: this.tracking
+              addTracking: this.tracking,
+              isSmtp: this.isSmtp,
+              smtpHost: this.smtpHost,
+              smtpUser: this.smtpUser,
+              smtpPassword: this.smtpPassword,
+              smtpSecure: this.smtpSecure,
+              smtpPort: this.smtpPort
             }).then(
             function success (response) {
               this.errorText = ''
@@ -139,13 +202,20 @@
               fromName: this.from_name,
               subject: this.headline,
               body: this.content,
-              addTracking: this.tracking
+              addTracking: this.tracking,
+              isSmtp: this.isSmtp,
+              smtpHost: this.smtpHost,
+              smtpUser: this.smtpUser,
+              smtpPassword: this.smtpPassword,
+              smtpSecure: this.smtpSecure,
+              smtpPort: this.smtpPort
             }).then(
             function success (response) {
               this.errorText = ''
               this.$router.push('/phishing/mailings')
             },
             function fail (response) {
+              window.scrollTo(0, 0)
               var result = JSON.parse(response.bodyText)
               // TODO: Local String, not out of response.
               this.errorText = result.message
